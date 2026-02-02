f2a200ef8bdca6964dca14fd386277fea2af99c2
kamasuto <kms1tt0@gmail.com>
2026-02-02 22:45:36 +0200

    Design change, Improvements in DeviceManager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.silicon"
-        minSdk = 24
+        minSdk = 25
         targetSdk = 36
         versionCode = 1
         versionName = "1.2"
